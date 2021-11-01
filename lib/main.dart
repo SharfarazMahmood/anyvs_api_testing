7fc89ca73cf05efdc78e2ca436bd8ab3feec82f4
SharfarazMahmood <sharfarazmahmood@gmail.com>
2021-11-01 20:20:54 +0600

    A splash screen with anyvas logo is added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:anyvas_api_testing/models/product_model.dart';
 import 'package:anyvas_api_testing/screens/product_details.dart';
+import 'package:anyvas_api_testing/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './providers/product_list_provider.dart';
@@ -31,7 +33,14 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: theme(),
         title: "Anyvas",
-        home: ProductsOverviewScreen(),
+        home:  AnimatedSplashScreen(
+          duration: 2000,
+          splash: Splash(),
+          splashTransition: SplashTransition.fadeTransition,
+          nextScreen: ProductsOverviewScreen(),
+          backgroundColor:const Color(0xff001a41),
+
+        ),
         routes: {
           ProductsOverviewScreen.routeName: (context) =>
               ProductsOverviewScreen(),
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'package:anyvas_api_testing/widgets/products_overview_screen.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    // _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 1500), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => ProductsOverviewScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const <Widget>[
+        Expanded(
+          child: Image(
+            image: AssetImage('assets/images/logo/anyvas-icon-Logo.png'),
+          ),
+        ),
+        Text(
+          "Anyvas",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Color(0xffe99800),
+          ),
+        ),
+      ],
+    );
+  }
+}
