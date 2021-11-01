57ec9a4bb2e757765b1f7522b2927f7f13e69773
SharfarazMahmood <sharfarazmahmood@gmail.com>
2021-11-01 15:30:36 +0600

    Making product list API call in a compute(Isotale) method(multi threading), converting API respose to a product list using async-await.

--- lib/providers/product_list_provider.dart
+++ lib/providers/product_list_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:anyvas_api_testing/models/product_model.dart';
@@ -13,68 +14,71 @@ class ProductListProvider with ChangeNotifier {
     return [..._items];
   }
 
-  List<ProductMdl> categoryItems({int? id}) {
-    return [..._items];
-  }
+  Future<void> getProducts({int? catId = 1}) async {
+    String responseData = await compute<int?, String>(fetchFromApi, catId);
 
-  Future<void> getProducts({int? id}) async {
-    var headers = {
-      'NST':
-          'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJOU1RfS0VZIjoidGVzdGFwaTEyM3Nha2hhdyJ9.l9txvKvpCrPsW78C9CFfUEVBbZcPpC7kBESRWBUthWjBG6dfP0YgrtoNKoe-PHExT_LGzYXoT1vvxGzWKxDGMA',
-      'Token':
-          'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJOU1RfS0VZIjoidGVzdGFwaTEyM3Nha2hhdyJ9.ca-7lHYgFnU_LXR_Q6_j3pIVb8oAkbn7kDonJn_4SepPhewJ6AHJyLUoITkAsIeOhakoePZ1bjq1rAb3f0GwrQ',
-      'DeviceId': 'DeviceId',
-      'Content-Type': 'application/json',
-    };
-    var request = http.Request(
-        'GET', Uri.parse('http://incap.bssoln.com/api/category/$id'));
-    request.body = json.encode({"Id": id});
-    // print('http://incap.bssoln.com/api/category/$id');
-    request.headers.addAll(headers);
-
-    http.StreamedResponse response = await request.send();
-
-    if (response.statusCode == 200) {
-      var responseData = await response.stream.bytesToString();
-      // print(responseData);
-      // log(responseData);
-
-      final extractedData = json.decode(responseData) as Map<String, dynamic>;
-      final productsData =
-          extractedData['Data']['CatalogProductsModel']['Products'] as List;
-      List<ProductMdl> productsList = [];
-      productsList = createProductObject(productsData);
-      _items = productsList;
-      notifyListeners();
-      // _items.forEach((element) {
-      //   print(element.name);
-      //   print(element.defaultPictureModel!.imageUrl);
-      // });
+    if (responseData == "-1") {
     } else {
-      print(response.reasonPhrase);
+      _items = await createProductObject(responseData);
     }
+    notifyListeners();
+    // _items.forEach((element) {
+    //   print(element.name);
+    //   print(element.defaultPictureModel!.imageUrl);
+    // });
+  }
+}
+
+/////////// using http request to get products data from API
+Future<String> fetchFromApi(int? id) async {
+  var headers = {
+    'NST':
+        'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJOU1RfS0VZIjoidGVzdGFwaTEyM3Nha2hhdyJ9.l9txvKvpCrPsW78C9CFfUEVBbZcPpC7kBESRWBUthWjBG6dfP0YgrtoNKoe-PHExT_LGzYXoT1vvxGzWKxDGMA',
+    'Token':
+        'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJOU1RfS0VZIjoidGVzdGFwaTEyM3Nha2hhdyJ9.ca-7lHYgFnU_LXR_Q6_j3pIVb8oAkbn7kDonJn_4SepPhewJ6AHJyLUoITkAsIeOhakoePZ1bjq1rAb3f0GwrQ',
+    'DeviceId': 'DeviceId',
+    'Content-Type': 'application/json',
+  };
+  var request = http.Request(
+      'GET', Uri.parse('http://incap.bssoln.com/api/category/$id'));
+  request.body = json.encode({"Id": id});
+  // print('http://incap.bssoln.com/api/category/$id');
+  request.headers.addAll(headers);
+
+  http.StreamedResponse response = await request.send();
+
+  if (response.statusCode == 200) {
+    var responseData = await response.stream.bytesToString();
+    return responseData;
+  } else {
+    print(response.reasonPhrase);
   }
 
-  List<ProductMdl> createProductObject(List<dynamic> productsData) {
-    List<ProductMdl> list = [];
+  return "-1";
+}
 
-    for (var i = 0; i < productsData.length; i++) {
-      var dpmData = productsData[i]['DefaultPictureModel'];
-      PictureModel dpm = PictureModel(
-        imageUrl: dpmData['ImageUrl'],
-        // thumbImageUrl: dpmData['ThumbImageUrl'],
-        fullSizeImageUrl: dpmData['FullSizeImageUrl'],
-        title: dpmData['Title'],
-        alternateText: dpmData['AlternateText'],
-      );
+////////// converting API resopnseData string to a product list
+Future<List<ProductMdl>> createProductObject(String responseData) async {
+  List<ProductMdl> list = [];
+  final extractedData = json.decode(responseData) as Map<String, dynamic>;
+  final productsData =
+      extractedData['Data']['CatalogProductsModel']['Products'] as List;
 
-      // log(dpm.imageUrl);
-      list.add(ProductMdl(
-        name: productsData[i]['Name'],
-        id: productsData[i]['Id'],
-        defaultPictureModel: dpm,
-      ));
-    }
-    return list;
+  for (var i = 0; i < productsData.length; i++) {
+    var dpmData = productsData[i]['DefaultPictureModel'];
+    PictureModel dpm = PictureModel(
+      imageUrl: dpmData['ImageUrl'],
+      fullSizeImageUrl: dpmData['FullSizeImageUrl'],
+      title: dpmData['Title'],
+      alternateText: dpmData['AlternateText'],
+    );
+
+    // log(dpm.imageUrl);
+    list.add(ProductMdl(
+      name: productsData[i]['Name'],
+      id: productsData[i]['Id'],
+      defaultPictureModel: dpm,
+    ));
   }
+  return list;
 }
