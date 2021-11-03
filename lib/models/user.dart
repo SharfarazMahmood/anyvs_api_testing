9ebf23829f8d9f37a3f3f92e1e70a6cdbfa1b308
SharfarazMahmood <sharfarazmahmood@gmail.com>
2021-11-03 19:25:02 +0600

    User model added to hold user data from backend.

---
+++ lib/models/user.dart
@@ -0,0 +1,15 @@
+class User {
+  String? email;
+  String? phone;
+  String? username;
+  String? firstName;
+  String? lastName;
+
+  User({
+    this.email,
+    this.phone,
+    this.username,
+    this.firstName,
+    this.lastName,
+  });
+}
