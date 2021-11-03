c14aaf42c5d86c2a3bf251417da87266096fc335
SharfarazMahmood <sharfarazmahmood@gmail.com>
2021-11-03 19:23:49 +0600

    An HttpException model added. It holds a list of error that might be returened from the backend.

--- lib/models/http_exception.dart
+++ lib/models/http_exception.dart
@@ -1,4 +1,8 @@
 class HttpException implements Exception {
+  static final errorList = [
+    "No customer account found",
+    "The credentials provided are incorrect",
+  ];
   final String message;
 
   HttpException(this.message);
