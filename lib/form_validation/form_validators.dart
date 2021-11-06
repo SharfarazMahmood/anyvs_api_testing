00ad0700bc94ca0938544e95b1a7c823d7e47739
SharfarazMahmood <sharfarazmahmood@gmail.com>
2021-11-06 22:50:16 +0600

    Form validation updated.

--- lib/form_validation/form_validators.dart
+++ lib/form_validation/form_validators.dart
@@ -1,7 +1,21 @@
 class FormValidators {
-    static String? emailorPhoneValidator(String? value) {
+  static nameValidator(String? value) {
     if (value!.isEmpty) {
-      return "Enter a phone of email !";
+      return "Enter a name";
+    }
+    return null;
+  }
+
+  static String? emailOrPhoneValidator(String? value) {
+    if (value!.isEmpty) {
+      return "Enter an email/phone";
+    }
+    return null;
+  }
+
+  static String? emailValidator(String? value) {
+    if (value!.isEmpty) {
+      return "Enter an email";
     }
     return null;
   }
@@ -10,9 +24,30 @@ class FormValidators {
     if (value!.isEmpty) {
       return "Enter a password";
     }
-    if (value.isEmpty || value.length < 5) {
+    if (value.length < 6) {
       return 'Password is too short !';
     }
     return null;
   }
+
+  static confirmPasswordValidator(String? value) {
+    if (value!.isEmpty) {
+      return "Re-enter password";
+    }
+    if (value.length < 6) {
+      return 'Password is too short !';
+    }
+    return null;
+  }
+
+  static String? phoneValidator(String? value) {
+    if (value!.isEmpty) {
+      return "Enter a phone number";
+    }
+    if (value.length != 11) {
+      return 'Invalid phone number';
+    }
+    return null;
+  }
+
 }
